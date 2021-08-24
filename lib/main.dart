0871f2bdd52a7eb762dbc931b391dfc0ed351e1a
Manjunath R K <rkmanjunath10000@gmail.com>
2021-08-24 21:38:31 +0530

    added resusable and weather cards files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'reusablecard.dart';
+import 'infocard.dart';
+import 'weathercard.dart';
 
 void main() {
   runApp(HomePage());
@@ -20,9 +23,124 @@ class _HomePageState extends State<HomePage> {
       theme: ThemeData(scaffoldBackgroundColor: kBackgroundColor),
       home: Scaffold(
         //backgroundColor: Color(0xFF070a30),
-        body: Column(
-          children: [
-            Expanded(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      //padding: EdgeInsets.only(top: 20.0),
+                      child: Center(
+                        child: Text(
+                          'San Fransisco',
+                          style: kHeadingText,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Container(
+                      //padding: EdgeInsets.only(top: 20.0),
+                      //padding: EdgeInsets.all(50.0),
+                      child: Center(
+                        child: Text(
+                          'May 28 2021',
+                          style: kDateTextStyle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                //flex: 4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ReusableButton(text: 'Forecast'),
+                    ReusableButton(text: 'Air Quality'),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image.asset(
+                  'icons/sun/8.png',
+                  width: 900.0,
+                  height: 900.0,
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InfoCard(
+                      headingText: 'Temp',
+                      text: '32°',
+                    ),
+                    InfoCard(
+                      headingText: 'Wind',
+                      text: '10 km/h',
+                    ),
+                    InfoCard(
+                      headingText: 'Humidity',
+                      text: '75%',
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //crossAxisAlignment: CrossAxisAlignment.baseline,
+                  children: [
+                    /*Padding(
+                      padding: EdgeInsets.all(15.0),
+                    ),*/
+                    Text(
+                      'Today',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'View Full report',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5.0),
+                width: double.infinity,
+                height: 100.0,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    WeatherCard(),
+                    WeatherCard(),
+                    WeatherCard(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* 
+Expanded(
               child: Container(
                 //padding: EdgeInsets.only(top: 20.0),
                 child: Center(
@@ -44,69 +162,4 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Expanded(
-              //flex: 4,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ReusableButton(text: 'Forecast'),
-                  ReusableButton(text: 'Air Quality'),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Image.asset(
-                'icons/sun/8.png',
-                width: 900.0,
-                height: 900.0,
-              ),
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InfoCard(
-                    headingText: 'Temp',
-                    text: '32°',
-                  ),
-                  InfoCard(
-                    headingText: 'Wind',
-                    text: '10 km/h',
-                  ),
-                  InfoCard(
-                    headingText: 'Humidity',
-                    text: '75%',
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ReusableButton extends StatelessWidget {
-  final String text;
-
-  ReusableButton({required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Colors.blue,
-      child: Container(
-        width: 85.0,
-        height: 30.0,
-        padding: EdgeInsets.all(7.0),
-        child: Center(
-            child: Text(
-          text,
-          style: kButtonTextStyle,
-        )),
-      ),
-    );
-  }
-}
+*/
