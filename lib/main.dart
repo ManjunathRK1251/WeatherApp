6a1cae361265da75d404da6f8fba608b9b7ed94d
Manjunath R K <rkmanjunath10000@gmail.com>
2021-08-26 19:50:21 +0530

    removed redundant import statements in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
-import 'reusablecard.dart';
-import 'infocard.dart';
-import 'weathercard.dart';
 import 'mainpage.dart';
 import 'navbar.dart';
 
