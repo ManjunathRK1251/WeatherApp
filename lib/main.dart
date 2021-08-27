51bf3f242fb9526ff25bb3be7b38f2ce5f5cdafe
Manojkc15 <manojkc9845@gmail.com>
2021-08-27 19:07:46 +0530

    commented CBNB, added BNB

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:weather_app/forecast_report.dart';
 import 'constants.dart';
@@ -15,13 +15,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
   PageController pageController = PageController();
   void onTapped(int index) {
-    pageController.animateToPage(index,
-        duration: Duration(milliseconds: 300), curve: Curves.ease);
     setState(() {
-      pageController.jumpToPage(index);
+      _selectedIndex = index;
+      pageController.animateToPage(index,
+          duration: Duration(milliseconds: 300), curve: Curves.ease);
     });
+    // pageController.jumpToPage(index);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._selectedIndex = page;
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -31,8 +45,9 @@ class _HomePageState extends State<HomePage> {
       theme: ThemeData(scaffoldBackgroundColor: kBackgroundColor),
       home: Scaffold(
         body: PageView(
-          physics: NeverScrollableScrollPhysics(),
+          // physics: NeverScrollableScrollPhysics(),
           controller: pageController,
+          onPageChanged: onPageChanged,
           children: [
             mainpage(),
             PickLocationpage(),
@@ -44,32 +59,48 @@ class _HomePageState extends State<HomePage> {
             )),
           ],
         ),
-        bottomNavigationBar: CurvedNavigationBar(
-          height: 53.0,
-          color: Colors.blue.withAlpha(7),
-          backgroundColor: Color(0xFF070a30),
-          buttonBackgroundColor: Colors.blue,
-          animationDuration: Duration(milliseconds: 200),
-          animationCurve: Curves.ease,
-          items: <Widget>[
-            Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.today,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.settings,
-              color: Colors.white,
-            ),
-          ],
-          onTap: onTapped,
+        // bottomNavigationBar: CurvedNavigationBar(
+        //   height: 53.0,
+        //   color: Colors.blue.withAlpha(7),
+        //   backgroundColor: Color(0xFF070a30),
+        //   buttonBackgroundColor: Colors.blue,
+        //   animationDuration: Duration(milliseconds: 200),
+        //   animationCurve: Curves.ease,
+        //   items: <Widget>[
+        //     Icon(
+        //       Icons.home,
+        //       color: Colors.white,
+        //     ),
+        //     Icon(
+        //       Icons.search,
+        //       color: Colors.white,
+        //     ),
+        //     Icon(
+        //       Icons.today,
+        //       color: Colors.white,
+        //     ),
+        //     Icon(
+        //       Icons.settings,
+        //       color: Colors.white,
+        //     ),
+        //   ],
+        //   onTap: onTapped,
+        // ),
+        bottomNavigationBar: Theme(
+          data: ThemeData(canvasColor: kBackgroundColor),
+          child: BottomNavigationBar(
+            backgroundColor: Color(0xFF070a30),
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.search),label: 'Search'),
+              BottomNavigationBarItem(icon: Icon(Icons.today),label: 'Today'),
+              BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'Settings'),
+            ],
+            onTap: onTapped,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.white,
+          ),
         ),
       ),
     );
